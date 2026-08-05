3d07ac631655cf7555cf30ffb1084019357f2f22
Mohammed shamlal kp <shamlalthegreat@gmail.com>
2026-08-05 19:29:18 +0530

    Time: 4 ms (94.89%), Space: 77.1 MB (86.28%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,5 +1,24 @@
 class Solution {
     public int maxArea(int[] height) {
-        
+             if (height == null || height.length == 0) {
+            return 0;
+        }
+        int result = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int a = Math.min(height[left], height[right]);
+            int b = right - left;
+            if (a * b > result) {
+                result = a * b;
+            }
+            if (height[left] == a) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return result;
     }
 }
