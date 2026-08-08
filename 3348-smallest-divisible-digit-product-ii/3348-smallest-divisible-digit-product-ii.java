fe5f007700f57b10bf7ba071787ca3634018a89e
Mohammed shamlal kp <shamlalthegreat@gmail.com>
2026-08-08 08:55:55 +0530

    Time: 14 ms (100%), Space: 47.8 MB (93.88%) - LeetHub

--- 3348-smallest-divisible-digit-product-ii/3348-smallest-divisible-digit-product-ii.java
+++ 3348-smallest-divisible-digit-product-ii/3348-smallest-divisible-digit-product-ii.java
@@ -1,157 +1,131 @@
-import java.util.*;
-
 class Solution {
-
-    // Contribution of each digit (1-9)
-    static final int[][] contrib = {
-            {0,0,0,0}, //0 unused
-            {0,0,0,0}, //1
-            {1,0,0,0}, //2
-            {0,1,0,0}, //3
-            {2,0,0,0}, //4
-            {0,0,1,0}, //5
-            {1,1,0,0}, //6
-            {0,0,0,1}, //7
-            {3,0,0,0}, //8
-            {0,2,0,0}  //9
-    };
-
-    String num;
-    int n;
-    HashMap<String, Boolean> memo;
+    int primes[] = new int[] { 2, 3, 5, 7 };
+    int maxPrime = primes[primes.length - 1];
 
     public String smallestNumber(String num, long t) {
+        int primeCount[] = new int[maxPrime + 1];
+        int numLength = num.length();
+        int minLength;
+        int firstZeroIndexFromLeft = 0;
 
-        int[] need = factorize(t);
-        if (need == null) return "-1";
-
-        this.num = num;
-
-        for (int len = num.length(); len <= num.length() + 30; len++) {
-
-            n = len;
-            memo = new HashMap<>();
-
-            StringBuilder ans = new StringBuilder();
-
-            boolean ok;
-
-            if (len == num.length()) {
-                ok = dfs(0, need[0], need[1], need[2], need[3], true, ans);
-            } else {
-                ok = dfsLonger(0, need[0], need[1], need[2], need[3], ans);
+        for (int prime : primes) {
+            while (t % prime == 0) {
+                t /= prime;
+                primeCount[prime]++;
             }
-
-            if (ok) return ans.toString();
         }
 
-        return "-1";
+        if (t != 1) {
+            return "-1";
+        }
+
+        minLength = getMinLength(primeCount);
+
+        if (numLength < minLength) {
+            return buildSuffix(primeCount, minLength, new char[minLength]);
+        }
+
+        char[] result = new char[numLength + 1];
+
+        for (int i = 0; firstZeroIndexFromLeft < numLength
+                && (result[++i] = num.charAt(firstZeroIndexFromLeft)) != '0'; firstZeroIndexFromLeft++) {
+            logNum(primeCount, result[i], -1);
+        }
+
+        if (getMinLength(primeCount) == 0) {
+            if (firstZeroIndexFromLeft == numLength) {
+                return num;
+            }
+            Arrays.fill(result, ++firstZeroIndexFromLeft, result.length, '1');
+            return new String(result, 1, numLength);
+        }
+
+        for (int last = numLength - 1, end = Math.min(firstZeroIndexFromLeft, last); end >= 0; end--) {
+            for (logNum(primeCount, result[end + 1], 1); ++result[end + 1] <= '9'; logNum(primeCount, result[end + 1], 1)) {
+                logNum(primeCount, result[end + 1], -1);
+                if (getMinLength(primeCount) <= last - end) {
+                    return buildSuffix(primeCount, last - end, result);
+                }
+            }
+        }
+
+        return buildSuffix(primeCount, result.length, result);
     }
 
-    // Factorize t into 2,3,5,7
-    private int[] factorize(long t) {
-
-        int[] cnt = new int[4];
-
-        while (t % 2 == 0) {
-            cnt[0]++;
-            t /= 2;
+    void logNum(int[] primeCount, int num, int value) {
+        if (num < '2') {
+            return;
         }
 
-        while (t % 3 == 0) {
-            cnt[1]++;
-            t /= 3;
+        if (num == '9') {
+            primeCount[3] += value << 1;
+        } else if (num == '4') {
+            primeCount[2] += value << 1;
+        } else if (num == '8') {
+            primeCount[2] += value * 3;
+        } else if (num == '6') {
+            primeCount[2] += value;
+            primeCount[3] += value;
+        } else {
+            primeCount[num - '0'] += value;
         }
-
-        while (t % 5 == 0) {
-            cnt[2]++;
-            t /= 5;
-        }
-
-        while (t % 7 == 0) {
-            cnt[3]++;
-            t /= 7;
-        }
-
-        if (t != 1) return null;
-
-        return cnt;
     }
 
-    // Same length (must be >= num)
-    private boolean dfs(int pos, int a, int b, int c, int d,
-                        boolean tight, StringBuilder ans) {
+    String buildSuffix(int[] primeCount, int targetLength, char[] result) {
+        int index = result.length;
 
-        if (a > (n - pos) * 3) return false;
-        if (b > (n - pos) * 2) return false;
-        if (c > (n - pos)) return false;
-        if (d > (n - pos)) return false;
-
-        if (pos == n)
-            return a == 0 && b == 0 && c == 0 && d == 0;
-
-        String key = pos + "#" + a + "#" + b + "#" + c + "#" + d + "#" + tight;
-        if (memo.containsKey(key))
-            return false;
-
-        int start = tight ? num.charAt(pos) - '0' : 1;
-
-        if (start == 0) start = 1;
-
-        for (int dig = start; dig <= 9; dig++) {
-
-            int na = Math.max(0, a - contrib[dig][0]);
-            int nb = Math.max(0, b - contrib[dig][1]);
-            int nc = Math.max(0, c - contrib[dig][2]);
-            int nd = Math.max(0, d - contrib[dig][3]);
-
-            ans.append((char) ('0' + dig));
-
-            if (dfs(pos + 1,
-                    na, nb, nc, nd,
-                    tight && dig == (num.charAt(pos) - '0'),
-                    ans))
-                return true;
-
-            ans.deleteCharAt(ans.length() - 1);
+        while (primeCount[3] > 1) {
+            primeCount[3] -= 2;
+            result[--index] = '9';
         }
 
-        memo.put(key, false);
-        return false;
+        while (primeCount[2] > 2) {
+            primeCount[2] -= 3;
+            result[--index] = '8';
+        }
+
+        while (primeCount[7]-- > 0) {
+            result[--index] = '7';
+        }
+
+        if (primeCount[2] > 0 && primeCount[3] > 0) {
+            result[--index] = '6';
+            primeCount[2]--;
+            primeCount[3]--;
+        }
+
+        while (primeCount[5]-- > 0) {
+            result[--index] = '5';
+        }
+
+        while (primeCount[2] > 1) {
+            primeCount[2] -= 2;
+            result[--index] = '4';
+        }
+
+        while (primeCount[3] > 0) {
+            primeCount[3]--;
+            result[--index] = '3';
+        }
+
+        while (primeCount[2] > 0) {
+            primeCount[2]--;
+            result[--index] = '2';
+        }
+
+        while (index + targetLength != result.length) {
+            result[--index] = '1';
+        }
+
+        return targetLength == result.length ? new String(result) : new String(result, 1, result.length - 1);
     }
 
-    // Length > original
-    private boolean dfsLonger(int pos, int a, int b, int c, int d,
-                              StringBuilder ans) {
+    int getMinLength(int[] primeCount) {
+        int count2 = Math.max(0, primeCount[2]);
+        int count3 = Math.max(0, primeCount[3]);
+        int count23 = (count3 & 1) + (count2 % 3);
 
-        if (a > (n - pos) * 3) return false;
-        if (b > (n - pos) * 2) return false;
-        if (c > (n - pos)) return false;
-        if (d > (n - pos)) return false;
-
-        if (pos == n)
-            return a == 0 && b == 0 && c == 0 && d == 0;
-
-        String key = "L#" + pos + "#" + a + "#" + b + "#" + c + "#" + d;
-        if (memo.containsKey(key))
-            return false;
-
-        for (int dig = 1; dig <= 9; dig++) {
-
-            int na = Math.max(0, a - contrib[dig][0]);
-            int nb = Math.max(0, b - contrib[dig][1]);
-            int nc = Math.max(0, c - contrib[dig][2]);
-            int nd = Math.max(0, d - contrib[dig][3]);
-
-            ans.append((char) ('0' + dig));
-
-            if (dfsLonger(pos + 1, na, nb, nc, nd, ans))
-                return true;
-
-            ans.deleteCharAt(ans.length() - 1);
-        }
-
-        memo.put(key, false);
-        return false;
+        return (count3 >> 1) + (count2 / 3) + Math.max(0, primeCount[7]) + Math.max(0, primeCount[5])
+                + (count23 == 3 ? 2 : count23 > 0 ? 1 : 0);
     }
 }
